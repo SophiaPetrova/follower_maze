b964da831091beea71918dc02abd53ee56f9dd58
Solon Aguiar <solon.liam.aguiar@gmail.com>
2015-09-10 13:36:11 -0700

    Failure scenarios for the UserClientSocketHandler

--- spec/socket/user_client_socket_handler_spec.rb
+++ spec/socket/user_client_socket_handler_spec.rb
@@ -6,16 +6,35 @@ describe :UserClientSocketHandler do
     let(:client_socket) { double('client_socket') }
     let(:user_client_socket) { double('user_client_socket') }
 
-    let(:protocol_message) { "123\r\n" }
-    let(:id) { '123' }
+    describe :errors do
+      it 'if cannot read the id from the client' do
+        expect(client_socket).to receive(:gets).and_return(nil)
 
-    it 'registers a new user client socket with the received ID in the client pool' do
-      expect(client_socket).to receive(:gets).and_return(protocol_message)
-      expect(UserClientSocket).to receive(:new).and_return(user_client_socket)
-      expect(client_pool).to receive(:register).with(id, user_client_socket)
+        handler = UserClientSocketHandler.new client_pool
+        expect { handler.handle(client_socket) } .to raise_error(UserClientIdNotProvidedError)
+      end
 
-      handler = UserClientSocketHandler.new client_pool
-      handler.handle client_socket
+      it 'if the id returned from the client is empty' do
+        expect(client_socket).to receive(:gets).and_return("\n")
+
+        handler = UserClientSocketHandler.new client_pool
+        expect { handler.handle(client_socket) } .to raise_error(UserClientIdNotProvidedError)
+      end
     end
+
+    describe :succeeds do
+      let(:protocol_message) { "123\r\n" }
+      let(:id) { '123' }
+
+      it 'registers a new user client socket with the received ID in the client pool' do
+        expect(client_socket).to receive(:gets).and_return(protocol_message)
+        expect(UserClientSocket).to receive(:new).and_return(user_client_socket)
+        expect(client_pool).to receive(:register).with(id, user_client_socket)
+
+        handler = UserClientSocketHandler.new client_pool
+        handler.handle client_socket
+      end
+    end
+
   end
 end
--- src/socket/user_client_socket_handler.rb
+++ src/socket/user_client_socket_handler.rb
@@ -4,7 +4,10 @@ class UserClientSocketHandler
   end
 
   def handle(client_socket)
-    client_id = client_socket.gets.strip
+    protocol_message = client_socket.gets
+    raise UserClientIdNotProvidedError.new, "Socked did not provide a client id" if protocol_message.nil? || protocol_message.strip.empty?
+
+    client_id = protocol_message.strip
     @client_pool.register client_id, UserClientSocket.new(client_socket)
   end
 end
