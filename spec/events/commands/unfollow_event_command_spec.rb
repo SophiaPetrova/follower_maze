97ea7fcae4b16d1dacdce83c82368e95584f4410
Solon Aguiar <solon.liam.aguiar@gmail.com>
2015-09-10 20:35:13 -0700

    UnfollowEventCommand class + tests

---
+++ spec/events/commands/unfollow_event_command_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe :UnfollowEventCommand do
+  let(:missing_sequence_number) { 'U|12|9' }
+  let(:negative_sequence_number) { '-1|U|12|9' }
+  let(:complete_command) { '1|U|12|9' }
+
+  context :parse do
+    describe 'when the command string does not match the format' do
+      it 'returns nil' do
+        expect(UnfollowEventCommand.parse(missing_sequence_number)).to be_nil
+      end
+    end
+
+    describe 'when the command string matches the format' do
+      it 'returns nil for invalid sequence numbers' do
+        expect(UnfollowEventCommand.parse(negative_sequence_number)).to be_nil
+      end
+
+      it 'returns a new follow command for valid commands' do
+        command = UnfollowEventCommand.parse(complete_command)
+
+        expect(command).to_not be_nil
+        expect(command.sequence).to eq(1)
+        expect(command.from_user).to eq(12)
+        expect(command.to_user).to eq(9)
+      end
+    end
+  end
+end
--- src/events/commands/unfollow_event_command.rb
+++ src/events/commands/unfollow_event_command.rb
@@ -1,3 +1,11 @@
 class UnfollowEventCommand < EventCommand
+  def self.parse(command)
+    return nil if REGEX.match(command).nil?
+    tokens = command.split @@command_token_spearator
 
+    UnfollowEventCommand.new(tokens[0].to_i, tokens[2].to_i, tokens[3].to_i)
+  end
+
+  private
+  REGEX = /^\d+\|U\|\d+\|\d+$/
 end
