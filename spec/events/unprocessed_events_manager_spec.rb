726c1ee985efa3e47d527d54fcc6d4d8b4e5455f
Solon Aguiar <solon.liam.aguiar@gmail.com>
2015-09-10 19:12:57 -0700

    UnprocessedEventsManager class to abstract the logic of maintaining the events buffer

--- spec/events/unprocessed_events_manager_spec.rb
+++ spec/events/unprocessed_events_manager_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe :UnprocessedEventsManager do
+  let(:event_command) { EventCommand.new 99 }
+  let(:other_event_command) { EventCommand.new 98 }
+
   context :add do
     it 'saves the event to the buffer' do
       unprocessed_events = UnprocessedEventsManager.new
-      unprocessed_events.add(EventCommand.new 99)
+      unprocessed_events.add(event_command)
 
       expect(unprocessed_events.size).to eq(1)
     end
 
     it 'replaces the event with a newest version of it' do
       unprocessed_events = UnprocessedEventsManager.new
-      event_command = EventCommand.new 99
 
       unprocessed_events.add(event_command)
       unprocessed_events.add(event_command)
@@ -20,12 +22,44 @@ describe :UnprocessedEventsManager do
 
     it 'differentiates different events by the sequence number' do
       unprocessed_events = UnprocessedEventsManager.new
-      event_command = EventCommand.new 99
-      other_event_command = EventCommand.new 98
 
       unprocessed_events.add(event_command)
       unprocessed_events.add(other_event_command)
       expect(unprocessed_events.size).to eq(2)
+    end
+  end
+
+  context :next_event do
+    it 'returns nil if the next event does not exist in the buffer' do
+      unprocessed_events = UnprocessedEventsManager.new
+
+      unprocessed_events.add(event_command)
+      expect(unprocessed_events.next_event event_command).to be_nil
+    end
+
+    it 'returns the next event if the next event exists in the buffer' do
+      unprocessed_events = UnprocessedEventsManager.new
+
+      unprocessed_events.add(event_command)
+      expect(unprocessed_events.next_event other_event_command).to_not be_nil
+    end
+  end
+
+  context :remove do
+    it 'does nothing if the event does not exist in the buffer' do
+      unprocessed_events = UnprocessedEventsManager.new
+      unprocessed_events.add(event_command)
+
+      unprocessed_events.remove(other_event_command)
+      expect(unprocessed_events.size).to eq(1)
+    end
+
+    it 'removes the element' do
+      unprocessed_events = UnprocessedEventsManager.new
+      unprocessed_events.add(event_command)
+
+      unprocessed_events.remove(EventCommand.new 99)
+      expect(unprocessed_events.size).to eq(0)
     end
   end
 end
--- src/events/unprocessed_events_manager.rb
+++ src/events/unprocessed_events_manager.rb
@@ -7,7 +7,16 @@ class UnprocessedEventsManager
     @unprocessed_events[event_command.sequence] = event_command
   end
 
+  def remove(event_command)
+    @unprocessed_events.delete event_command.sequence
+  end
+
   def size
     @unprocessed_events.keys.length
+  end
+
+  def next_event(event)
+    return nil if !@unprocessed_events.has_key? event.next_command_sequence
+    @unprocessed_events[event.next_command_sequence]
   end
 end
