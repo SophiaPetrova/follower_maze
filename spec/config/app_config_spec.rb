f3218474e5ef63f80a1216e722f0bf5f3c49754a
Solon Aguiar <solon.liam.aguiar@gmail.com>
2015-09-12 07:39:57 -0700

    New methods for the socket ports in app config

--- spec/config/app_config_spec.rb
+++ spec/config/app_config_spec.rb
@@ -2,18 +2,57 @@ require 'spec_helper'
 
 describe :AppConfig do
   context :is_production? do
+    let(:environment_variable) { 'TEST' }
 
-    context 'when the there is a TEST environment variable' do
+    context 'when the there is a environment variable' do
       it 'returns false' do
-        ENV['TEST'] = 'true'
+        ENV[environment_variable] = 'true'
         expect(AppConfig.production?).to be false
       end
     end
 
-    context 'when the there is no TEST environment variable' do
+    context 'when the there is not an environment variable' do
       it 'returns true' do
-        ENV = []
+        ENV.delete environment_variable
         expect(AppConfig.production?).to be true
+      end
+    end
+  end
+
+  context :events_port do
+    let(:event_listener_port) { '12345' }
+    let(:environment_variable) { 'eventListenerPort' }
+
+    context 'when the there is a envetListenerPort environment variable' do
+      it 'returns the value of the variable' do
+        ENV[environment_variable] = event_listener_port
+        expect(AppConfig.events_port).to eq(event_listener_port.to_i)
+      end
+    end
+
+    context 'when the there is not an environment variable' do
+      it 'returns 9090' do
+        ENV.delete environment_variable
+        expect(AppConfig.events_port).to eq(9090)
+      end
+    end
+  end
+
+  context :clients_port do
+    let(:clients_port) { '12345' }
+    let(:environment_variable) { 'clientListenerPort' }
+
+    context 'when the there is a envetListenerPort environment variable' do
+      it 'returns the value of the variable' do
+        ENV[environment_variable] = clients_port
+        expect(AppConfig.clients_port).to eq(clients_port.to_i)
+      end
+    end
+
+    context 'when the there is not an environment variable' do
+      it 'returns 9099' do
+        ENV.delete environment_variable
+        expect(AppConfig.clients_port).to eq(9099)
       end
     end
   end
--- src/config/app_config.rb
+++ src/config/app_config.rb
@@ -3,6 +3,16 @@ class AppConfig
     !ENV.include? PRODUCTION_VARIABLE
   end
 
+  def self.events_port
+    ENV[EVENTS_PORT].nil? ? 9090 : ENV[EVENTS_PORT].to_i
+  end
+
+  def self.clients_port
+    ENV[CLIENTS_PORT].nil? ? 9099 : ENV[CLIENTS_PORT].to_i
+  end
+
   private
   PRODUCTION_VARIABLE = 'TEST'.freeze
+  EVENTS_PORT = 'eventListenerPort'.freeze
+  CLIENTS_PORT = 'clientListenerPort'.freeze
 end
